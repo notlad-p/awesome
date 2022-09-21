794f1ea88a057c934703ce9f5db9b326262883ee
Dalton Perkins <contact@daltonp.dev>
2022-09-20 21:44:47 -0400

    updated configuration

--- configuration/apps.lua
+++ configuration/apps.lua
@@ -1,12 +1,19 @@
 return {
 	default = {
-		terminal = "alacritty",
+		terminal = "wezterm",
 		editor = os.getenv("EDITOR") or "editor",
 		web_browser = "firefox",
 	},
 	run_on_start_up = {
+		-- https://askubuntu.com/questions/1170247/how-do-i-solve-screen-tearing-on-ubuntu-18-04-with-nvidia
+		-- TODO: Add tutorial above to solve nvidia screen tearing
+		-- TODO: USE ASYNC SHELL? (won't work with normal shell)
+		-- "\"$(xrandr | sed -nr '/(S+) connected (primary )?([0-9]+x[0-9]+)(+S+).*/{ s//\1: \3 \4 { ForceFullCompositionPipeline = On }, /; H}; ${ g; s/\n//g; s/, $//; p }')\"",
 		"picom --experimental-backends -b",
 		-- setup my monitor positions
 		"xrandr --output DP-2 --auto --output DP-4 --auto --right-of DP-2 --output HDMI-0 --auto --right-of DP-4",
+	},
+	util = {
+		screenshot_directory = "~/Pictures/",
 	},
 }
--- configuration/layouts.lua
+++ configuration/layouts.lua
@@ -2,7 +2,6 @@ local awful = require("awful")
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
-	-- awful.layout.suit.floating,
 	awful.layout.suit.tile,
 	awful.layout.suit.tile.left,
 	awful.layout.suit.tile.bottom,
@@ -10,6 +9,7 @@ awful.layout.layouts = {
 	awful.layout.suit.fair,
 	awful.layout.suit.max,
 	awful.layout.suit.max.fullscreen,
+	awful.layout.suit.floating,
 	-- awful.layout.suit.fair.horizontal,
 	-- awful.layout.suit.spiral,
 	-- awful.layout.suit.spiral.dwindle,
