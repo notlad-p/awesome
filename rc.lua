4e51398ea0f6fc1a6039fc17508b304815f8edc3
Dalton Perkins <contact@daltonp.dev>
2023-01-19 09:08:04 -0500

    reformatted require statements

--- rc.lua
+++ rc.lua
@@ -2,40 +2,40 @@
 -- found (e.g. lgi). If LuaRocks is not installed, do nothing.
 pcall(require, "luarocks.loader")
 
-local gears = require("gears")
-local awful = require("awful")
-require("awful.autofocus")
-local beautiful = require("beautiful")
+local gears = require "gears"
+local awful = require "awful"
+require "awful.autofocus"
+local beautiful = require "beautiful"
 
 -- Enable hotkeys help widget for VIM and other apps
 -- when client with a matching name is opened:
-require("awful.hotkeys_popup.keys")
+require "awful.hotkeys_popup.keys"
 
 -- error handling
-require("error")
+require "error"
 
 -- Themes define colours, icons, font and wallpapers.
 beautiful.init(gears.filesystem.get_configuration_dir() .. "theme/theme.lua")
 
 -- setup layouts (needs to come before 'ui')
-require("configuration.layouts")
+require "configuration.layouts"
 
 -- set up ui
 require("ui").setup()
 
 -- mouse bindings
 root.buttons(gears.table.join(
-	-- awful.button({}, 3, function()
-	-- 	mymainmenu:toggle()
-	-- end),
-	awful.button({}, 4, awful.tag.viewnext),
-	awful.button({}, 5, awful.tag.viewprev)
+  -- awful.button({}, 3, function()
+  -- 	mymainmenu:toggle()
+  -- end),
+  awful.button({}, 4, awful.tag.viewnext),
+  awful.button({}, 5, awful.tag.viewprev)
 ))
 
 -- set keys
 root.keys(require("configuration.keys").setup())
 -- setup client
-require("configuration.client")
+require "configuration.client"
 
 -- MODULES
-require("module.auto-start")
+require "module.auto-start"
