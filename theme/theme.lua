a53165000caaad79296e9ca5d56245289d238dc8
Dalton Perkins <contact@daltonp.dev>
2023-01-20 15:40:51 -0500

    changed imaged / icons directory and removed clutter in theme.lua

--- theme/theme.lua
+++ theme/theme.lua
@@ -1,12 +1,3 @@
----------------------------
--- Default awesome theme --
----------------------------
--- TODO: CHANGE ALL SVG ICONS TO USE GEARS.COLOR - put all icons in icons/
--- directory
--- use gears.color.recolor_image to change svg icon colors instead of
--- using totally different svgs
--- https://www.reddit.com/r/awesomewm/comments/baga7l/change_color_of_svg_icon/
-
 local theme_assets = require "beautiful.theme_assets"
 local xresources = require "beautiful.xresources"
 local dpi = xresources.apply_dpi
@@ -18,17 +9,25 @@ local user_config_path = gfs.get_configuration_dir()
 
 local theme = {}
 
--- selected colorscheme
+---------------------------------
+-- Stuff that might be customized
+---------------------------------
+-- colorscheme
 local colorscheme = "gruvbox"
+-- wallpaper
+theme.wallpaper = user_config_path .. "images/wallpapers/2.jpg"
+-- font
+theme.font = "FiraCode NF 8.5"
 
+----------------------
+-- COLORS & WIDGETS
+----------------------
 -- get colorscheme values & set them in theme table
 local schemes = require "theme.colorschemes"
 local selected_scheme = schemes[colorscheme]
 for key, value in pairs(selected_scheme) do
   theme[key] = value
 end
-
-theme.font = "FiraCode NF 8.5"
 
 theme.bg_normal = theme.bg_0
 theme.bg_focus = theme.bg_3
@@ -49,48 +48,8 @@ theme.border_focus = theme.light_grey
 theme.border_marked = theme.red
 
 -- wibar theme
--- theme.wibar_bg = theme.bg_0
-theme.wibar_bg = "#00000000"
+theme.wibar_bg = "#00000000" -- transparent for separated look
 theme.wibar_fg = theme.fg
--- theme.wibar_opacity = 0.5
-
--- toggle panel
-theme.margin_outside = 24
-theme.margin_inside = 16
--- profile picture
-theme.pfp = user_config_path .. "theme/images/pfp.png"
--- icons
-theme.plane = user_config_path .. "theme/icons/toggle-panel/plane.svg"
-theme.plane_toggled = user_config_path .. "theme/icons/toggle-panel/plane-toggled.svg"
-theme.bluetooth = user_config_path .. "theme/icons/toggle-panel/bluetooth.svg"
-theme.bluetooth_toggled = user_config_path .. "theme/icons/toggle-panel/bluetooth-toggled.svg"
-theme.wifi = user_config_path .. "theme/icons/toggle-panel/wifi.svg"
-theme.wifi_toggled = user_config_path .. "theme/icons/toggle-panel/wifi-toggled.svg"
-theme.crop = user_config_path .. "theme/icons/toggle-panel/crop.svg"
-theme.float = user_config_path .. "theme/icons/toggle-panel/float.svg"
-theme.float_toggled = user_config_path .. "theme/icons/toggle-panel/float-toggled.svg"
-theme.notifications = user_config_path .. "theme/icons/toggle-panel/notifications.svg"
-theme.notifications_toggled = user_config_path .. "theme/icons/toggle-panel/notifications-toggled.svg"
--- silders
-theme.volume = user_config_path .. "theme/icons/toggle-panel/volume.svg"
-theme.brightness = user_config_path .. "theme/icons/toggle-panel/brightness.svg"
--- music player
-theme.backward_fg = user_config_path .. "theme/icons/toggle-panel/backward.svg"
-theme.play_fg = user_config_path .. "theme/icons/toggle-panel/play.svg"
-theme.forward_fg = user_config_path .. "theme/icons/toggle-panel/forward.svg"
-theme.pause_fg = user_config_path .. "theme/icons/toggle-panel/pause.svg"
-
--- There are other variable sets
--- overriding the default one when
--- defined, the sets are:
--- taglist_[bg|fg]_[focus|urgent|occupied|empty|volatile]
--- tasklist_[bg|fg]_[focus|urgent]
--- titlebar_[bg|fg]_[normal|focus]
--- tooltip_[font|opacity|fg_color|bg_color|border_width|border_color]
--- mouse_finder_[color|timeout|animate_timeout|radius|factor]
--- prompt_[fg|bg|fg_cursor|bg_cursor|font]
--- hotkeys_[bg|fg|border_width|border_color|shape|opacity|modifiers_fg|label_bg|label_fg|group_margin|font|description_font]
--- Example:
 
 -- titlebar styles
 theme.titlebar_bg_normal = theme.bg_d
@@ -110,101 +69,64 @@ theme.tasklist_plain_task_name = true
 theme.taglist_bg_focus = "#00000000"
 theme.taglist_icon_occupied = "#93a4c3"
 
--- prompt cursor styles
-theme.prompt_bg_cursor = theme.grey
-theme.prompt_fg_cursor = theme.cyan
-
--- Generate taglist squares: (little squares that indicate if a client is
--- present in a tag)
--- local taglist_square_size = dpi(4)
--- theme.taglist_squares_sel = theme_assets.taglist_squares_sel(taglist_square_size, theme.fg_normal)
--- theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(taglist_square_size, theme.fg_normal)
-
--- Variables set for theming notifications:
--- theme.notification_bg = theme.bg_d
--- theme.notification_fg = theme.fg
--- theme.notification_shape = function(cr, width, height)
--- 	gears.shape.rounded_rect(cr, width, height, 10)
--- end
--- notification_font
--- notification_[width|height|margin]
--- notification_[border_color|border_width|shape|opacity]
-
--- Variables set for theming the menu:
--- menu_[bg|fg]_[normal|focus]
--- menu_[border_color|border_width]
+-- right click menu
 theme.menu_submenu_icon = themes_path .. "default/submenu.png"
 theme.menu_height = dpi(15)
 theme.menu_width = dpi(100)
 
--- You can add as many variables as
--- you wish and access them by using
--- beautiful.variable in your rc.lua
---theme.bg_widget = "#cc0000"
+-- custom margin rules
+theme.margin_outside = 24
+theme.margin_inside = 16
 
--- Define the image to load
--- theme.titlebar_close_button_normal = themes_path .. "default/titlebar/close_normal.png"
--- theme.titlebar_close_button_focus = themes_path .. "default/titlebar/close_focus.png"
-theme.titlebar_close_button_normal = user_config_path .. "theme/icons/titlebar/normal.svg"
-theme.titlebar_close_button_focus = user_config_path .. "theme/icons/titlebar/close.svg"
+----------------------
+-- IMAGES
+----------------------
 
--- theme.titlebar_minimize_button_normal = user_config_path .. "icons/minimize.svg"
--- theme.titlebar_minimize_button_focus = user_config_path .. "icons/minimize.svg"
-theme.titlebar_minimize_button_normal = user_config_path .. "theme/icons/titlebar/normal.svg"
-theme.titlebar_minimize_button_focus = user_config_path .. "theme/icons/titlebar/minimize.svg"
+-- profile picture
+theme.pfp = user_config_path .. "images/pfp.png"
 
--- theme.titlebar_ontop_button_normal_inactive = themes_path .. "default/titlebar/ontop_normal_inactive.png"
--- theme.titlebar_ontop_button_focus_inactive = themes_path .. "default/titlebar/ontop_focus_inactive.png"
--- theme.titlebar_ontop_button_normal_active = themes_path .. "default/titlebar/ontop_normal_active.png"
--- theme.titlebar_ontop_button_focus_active = themes_path .. "default/titlebar/ontop_focus_active.png"
---
--- theme.titlebar_sticky_button_normal_inactive = themes_path .. "default/titlebar/sticky_normal_inactive.png"
--- theme.titlebar_sticky_button_focus_inactive = themes_path .. "default/titlebar/sticky_focus_inactive.png"
--- theme.titlebar_sticky_button_normal_active = themes_path .. "default/titlebar/sticky_normal_active.png"
--- theme.titlebar_sticky_button_focus_active = themes_path .. "default/titlebar/sticky_focus_active.png"
+----------------------
+-- ICONS
+----------------------
+local icons_path = user_config_path .. "images/icons/"
+local recolor_image = gears.color.recolor_image
 
-theme.titlebar_floating_button_normal_inactive = user_config_path .. "theme/icons/titlebar/normal.svg"
-theme.titlebar_floating_button_focus_inactive = user_config_path .. "theme/icons/titlebar/grid-normal.svg"
-theme.titlebar_floating_button_normal_active = user_config_path .. "theme/icons/titlebar/normal-active.svg"
-theme.titlebar_floating_button_focus_active = user_config_path .. "theme/icons/titlebar/grid-active.svg"
+-- built-in icons
+-- these are built-in icons that need to be set and recolored here
+theme.awesome_icon = theme_assets.awesome_icon(theme.menu_height, theme.bg_focus, theme.fg_focus)
 
-theme.titlebar_maximized_button_normal_inactive = user_config_path .. "theme/icons/titlebar/normal.svg"
-theme.titlebar_maximized_button_focus_inactive = user_config_path .. "theme/icons/titlebar/maximize-normal.svg"
-theme.titlebar_maximized_button_normal_active = user_config_path .. "theme/icons/titlebar/normal-active.svg"
-theme.titlebar_maximized_button_focus_active = user_config_path .. "theme/icons/titlebar/maximize-active.svg"
+-- titlebar
+local circle = icons_path .. "circle.svg"
+local circle_filled = icons_path .. "circle-filled.svg"
+local circle_grey = recolor_image(circle, theme.grey)
 
--- set wallpaper
-theme.wallpaper = user_config_path .. "theme/wallpapers/2.jpg"
+theme.titlebar_close_button_normal = circle_grey
+theme.titlebar_close_button_focus = recolor_image(circle, theme.red)
 
--- add other images
-theme.logout_image = user_config_path .. "theme/wallpapers/girl-with-umbrella.jpg"
+theme.titlebar_minimize_button_normal = circle_grey
+theme.titlebar_minimize_button_focus = recolor_image(circle, theme.yellow)
 
--- You can use your own layout icons like this:
-theme.layout_fairv = user_config_path .. "theme/icons/layouts/fair.svg"
-theme.layout_max = user_config_path .. "theme/icons/layouts/maximize.svg"
-theme.layout_fullscreen = user_config_path .. "theme/icons/layouts/fullscreen.svg"
-theme.layout_tile = user_config_path .. "theme/icons/layouts/tile.svg"
-theme.layout_tileleft = user_config_path .. "theme/icons/layouts/tile-left.svg"
-theme.layout_tilebottom = user_config_path .. "theme/icons/layouts/tile-bottom.svg"
-theme.layout_tiletop = user_config_path .. "theme/icons/layouts/tile-top.svg"
-theme.layout_floating = user_config_path .. "theme/icons/layouts/float.svg"
+theme.titlebar_floating_button_normal_inactive = circle_grey
+theme.titlebar_floating_button_focus_inactive = recolor_image(circle, theme.purple)
+theme.titlebar_floating_button_normal_active = recolor_image(circle_filled, theme.purple)
+theme.titlebar_floating_button_focus_active = recolor_image(circle_filled, theme.purple)
 
--- wibar icons
-theme.music = user_config_path .. "theme/icons/misc/music.svg"
-theme.calendar = user_config_path .. "theme/icons/misc/calendar.svg"
-theme.clock = user_config_path .. "theme/icons/misc/clock.svg"
-theme.power = user_config_path .. "theme/icons/misc/power.svg"
-theme.backward = user_config_path .. "theme/icons/misc/backward.svg"
-theme.play = user_config_path .. "theme/icons/misc/play.svg"
-theme.forward = user_config_path .. "theme/icons/misc/forward.svg"
+theme.titlebar_maximized_button_normal_inactive = circle_grey
+theme.titlebar_maximized_button_focus_inactive = recolor_image(circle, theme.blue)
+theme.titlebar_maximized_button_normal_active = recolor_image(circle_filled, theme.blue)
+theme.titlebar_maximized_button_focus_active = recolor_image(circle_filled, theme.blue)
 
--- power panel icons
-theme.power_light = user_config_path .. "theme/icons/misc/power-light.svg"
-theme.lock = user_config_path .. "theme/icons/power-panel/lock.svg"
-theme.logout = user_config_path .. "theme/icons/power-panel/logout.svg"
-theme.restart = user_config_path .. "theme/icons/power-panel/restart.svg"
-theme.sleep = user_config_path .. "theme/icons/power-panel/sleep.svg"
-
+-- layouts
+local layouts_path = icons_path .. "layouts/"
+theme.layout_fairv = recolor_image(layouts_path .. "fair.svg", theme.bg_0)
+theme.layout_max = recolor_image(layouts_path .. "maximize.svg", theme.bg_0)
+theme.layout_fullscreen = recolor_image(layouts_path .. "fullscreen.svg", theme.bg_0)
+theme.layout_tile = recolor_image(layouts_path .. "tile.svg", theme.bg_0)
+theme.layout_tileleft = recolor_image(layouts_path .. "tile-left.svg", theme.bg_0)
+theme.layout_tilebottom = recolor_image(layouts_path .. "tile-bottom.svg", theme.bg_0)
+theme.layout_tiletop = recolor_image(layouts_path .. "tile-top.svg", theme.bg_0)
+theme.layout_floating = recolor_image(layouts_path .. "float.svg", theme.bg_0)
+-- Unused layouts:
 -- theme.layout_fairh = themes_path .. "default/layouts/fairhw.png"
 -- theme.layout_magnifier = themes_path .. "default/layouts/magnifierw.png"
 -- theme.layout_spiral = themes_path .. "default/layouts/spiralw.png"
@@ -214,7 +136,57 @@ theme.sleep = user_config_path .. "theme/icons/power-panel/sleep.svg"
 -- theme.layout_cornersw = themes_path .. "default/layouts/cornersww.png"
 -- theme.layout_cornerse = themes_path .. "default/layouts/cornersew.png"
 
--- Generate Awesome icon:
+-- Custom icons table representing their location in the `images/icons/` directory
+--
+-- To add an icon, copy the svg icon somewhere in `images/icons/`,
+-- then put the file name in this table (minus the .svg extension)
+--
+-- You can even add a key to the icon that will then be used as the key for that icon
+-- in the `theme` table. There's an example in the table.
+--
+-- THIS METHOD DOES NOT WORK 2 SUBDIRECTORIES DOWN, ONLY ONE
+local custom_icons = {
+  power = {
+    "lock",
+    "logout",
+    "power",
+    "restart",
+    "sleep",
+  },
+  "backward",
+  "bluetooth",
+  "brightness",
+  "calendar",
+  "clock",
+  -- time = "clock", -- The clock icon will need to be referenced as `time` now
+  "crop",
+  "float",
+  "forward",
+  "music",
+  "notifications",
+  "pause",
+  "plane",
+  "play",
+  "volume",
+  "wifi",
+}
+
+-- loop icons table & add key & path to `theme` table
+for key, value in pairs(custom_icons) do
+  -- if value is a table (sub-directory), loop it
+  if type(value) == "table" then
+    for key_2, value_2 in pairs(value) do
+      if type(key_2) == "number" then
+        theme[value_2] = icons_path .. key .. "/" .. value_2 .. ".svg"
+      else
+        theme[key_2] = icons_path .. key .. "/" .. value_2 .. ".svg"
+      end
+    end
+  else
+    theme[value] = icons_path .. value .. ".svg"
+  end
+end
+
 theme.awesome_icon = theme_assets.awesome_icon(theme.menu_height, theme.bg_focus, theme.fg_focus)
 
 -- Define the icon theme for application icons. If not set then the icons
@@ -222,5 +194,3 @@ theme.awesome_icon = theme_assets.awesome_icon(theme.menu_height, theme.bg_focus
 theme.icon_theme = nil
 
 return theme
-
--- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80
--- ui/top-panel/weather.lua
+++ ui/top-panel/weather.lua
@@ -1,126 +1,126 @@
-local wibox = require("wibox")
-local filesystem = require("gears.filesystem")
-local awful = require("awful")
-local naughty = require("naughty")
-local beautiful = require("beautiful")
-local gears = require("gears")
+local wibox = require "wibox"
+local filesystem = require "gears.filesystem"
+local awful = require "awful"
+local naughty = require "naughty"
+local beautiful = require "beautiful"
+local gears = require "gears"
 
-local json = require("module.json")
+local json = require "module.json"
 
-local icon_dir = filesystem.get_configuration_dir() .. "theme/icons/weather/"
+local icon_dir = filesystem.get_configuration_dir() .. "images/icons/weather/"
 local config = require("configuration.config").widget.weather
 
 local M = {}
 
 M.setup = function()
-	-- Possible icon values returned from OWM
-	local icons = {
-		["01d"] = "clear-sky",
-		["02d"] = "few-clouds",
-		["03d"] = "scattered-clouds",
-		["04d"] = "scattered-clouds", -- indicator is for 'broken-clouds' but I reused scattered-clouds because it's the same
-		["09d"] = "shower-rain",
-		["10d"] = "rain",
-		["11d"] = "thunderstorm",
-		["13d"] = "snow",
-		["50d"] = "mist",
-		["01n"] = "clear-sky-night",
-		["02n"] = "few-clouds-night",
-		["03n"] = "scattered-clouds",
-		["04n"] = "scattered-clouds",
-		["09n"] = "shower-rain",
-		["10n"] = "rain-night",
-		["11n"] = "thunderstorm",
-		["13n"] = "snow",
-		["50n"] = "mist",
-	}
+  -- Possible icon values returned from OWM
+  local icons = {
+    ["01d"] = "clear-sky",
+    ["02d"] = "few-clouds",
+    ["03d"] = "scattered-clouds",
+    ["04d"] = "scattered-clouds", -- indicator is for 'broken-clouds' but I reused scattered-clouds because it's the same
+    ["09d"] = "shower-rain",
+    ["10d"] = "rain",
+    ["11d"] = "thunderstorm",
+    ["13d"] = "snow",
+    ["50d"] = "mist",
+    ["01n"] = "clear-sky-night",
+    ["02n"] = "few-clouds-night",
+    ["03n"] = "scattered-clouds",
+    ["04n"] = "scattered-clouds",
+    ["09n"] = "shower-rain",
+    ["10n"] = "rain-night",
+    ["11n"] = "thunderstorm",
+    ["13n"] = "snow",
+    ["50n"] = "mist",
+  }
 
-	local icon_widget = wibox.widget({
-		id = "icon",
-		widget = wibox.widget.imagebox,
-	})
+  local icon_widget = wibox.widget {
+    id = "icon",
+    widget = wibox.widget.imagebox,
+  }
 
-	local temp_widget = wibox.widget({
-		id = "temp",
-		widget = wibox.widget.textbox,
-	})
+  local temp_widget = wibox.widget {
+    id = "temp",
+    widget = wibox.widget.textbox,
+  }
 
-	local weather_widget = wibox.widget({
-		{
-			{
-				{
-					icon_widget,
-					widget = wibox.container.margin,
-					left = 5,
-					right = 5,
-					top = 5,
-					bottom = 5,
-				},
-				bg = beautiful.bg_blue,
-				widget = wibox.container.background,
-				shape = function(cr, width, height)
-					gears.shape.partially_rounded_rect(cr, width, height, true, false, false, true, 7)
-				end,
-			},
-			{
-				{
-					temp_widget,
-					fg = beautiful.bg_blue,
-					widget = wibox.container.background,
-				},
-				widget = wibox.container.margin,
-				top = 5,
-				bottom = 5,
-				right = 10,
-				left = 10,
-			},
-			widget = wibox.layout.align.horizontal,
-		},
-		bg = beautiful.bg_3,
-		widget = wibox.container.background,
-		shape = function(cr, width, height)
-			gears.shape.rounded_rect(cr, width, height, 7)
-		end,
-	})
+  local weather_widget = wibox.widget {
+    {
+      {
+        {
+          icon_widget,
+          widget = wibox.container.margin,
+          left = 5,
+          right = 5,
+          top = 5,
+          bottom = 5,
+        },
+        bg = beautiful.blue,
+        widget = wibox.container.background,
+        shape = function(cr, width, height)
+          gears.shape.partially_rounded_rect(cr, width, height, true, false, false, true, 7)
+        end,
+      },
+      {
+        {
+          temp_widget,
+          fg = beautiful.blue,
+          widget = wibox.container.background,
+        },
+        widget = wibox.container.margin,
+        top = 5,
+        bottom = 5,
+        right = 10,
+        left = 10,
+      },
+      widget = wibox.layout.align.horizontal,
+    },
+    bg = beautiful.bg_3,
+    widget = wibox.container.background,
+    shape = function(cr, width, height)
+      gears.shape.rounded_rect(cr, width, height, 7)
+    end,
+  }
 
-	local function show_warning(message)
-		naughty.notify({
-			preset = naughty.config.presets.critical,
-			-- title = LCLE.warning_title,
-			text = message,
-		})
-	end
+  local function show_warning(message)
+    naughty.notify {
+      preset = naughty.config.presets.critical,
+      -- title = LCLE.warning_title,
+      text = message,
+    }
+  end
 
-	-- local GET_FORECAST_CMD = [[bash -c "curl -s --show-error -X GET '%s'"]]
-	local GET_FORECAST_CMD = "curl -X GET '%s'"
-	-- Open Weather Map API URL
-	local url = "https://api.openweathermap.org/data/2.5/weather?q="
-		.. config.city
-		.. "&units="
-		.. config.units
-		.. "&appid="
-		.. config.key
+  -- local GET_FORECAST_CMD = [[bash -c "curl -s --show-error -X GET '%s'"]]
+  local GET_FORECAST_CMD = "curl -X GET '%s'"
+  -- Open Weather Map API URL
+  local url = "https://api.openweathermap.org/data/2.5/weather?q="
+    .. config.city
+    .. "&units="
+    .. config.units
+    .. "&appid="
+    .. config.key
 
-	-- callback after the command output is received
-	local callback = function(widget, stdout, stderr)
-		-- show_warning(stderr)
-		-- show_warning(stdout)
+  -- callback after the command output is received
+  local callback = function(widget, stdout, stderr)
+    -- show_warning(stderr)
+    -- show_warning(stdout)
 
-		-- decoded json from GET request
-		local result = json.decode(stdout)
+    -- decoded json from GET request
+    local result = json.decode(stdout)
 
-		-- set icon
-		local icon_code = result.weather[1].icon
-		icon_widget.image = icon_dir .. icons[icon_code] .. ".svg"
+    -- set icon
+    local icon_code = result.weather[1].icon
+    icon_widget.image = icon_dir .. icons[icon_code] .. ".svg"
 
-		-- set temperature
-		local temp_num = math.floor(result.main.temp)
-		temp_widget.markup = tostring(temp_num) .. "°<sup>F</sup>"
-	end
+    -- set temperature
+    local temp_num = math.floor(result.main.temp)
+    temp_widget.markup = tostring(temp_num) .. "°<sup>F</sup>"
+  end
 
-	-- awful.widget.watch(string.format(GET_FORECAST_CMD, url), config.update_interval, callback)
+  -- awful.widget.watch(string.format(GET_FORECAST_CMD, url), config.update_interval, callback)
 
-	return weather_widget
+  return weather_widget
 end
 
 return M
