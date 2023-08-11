066be24c5960d23745dd53b807b109a63373944d
Dalton <contact@daltonp.dev>
2023-08-11 17:59:07 -0400

    separated slider and slider icon widgets

---
+++ ui/toggle-panel/sliders/slider-icon.lua
@@ -0,0 +1,30 @@
+local wibox = require "wibox"
+local gears = require "gears"
+local beautiful = require "beautiful"
+
+return function(image)
+  local recolored_image = gears.color.recolor_image(image, beautiful.fg)
+  return wibox.widget {
+    {
+      {
+        {
+          forced_height = 16,
+          forced_width = 16,
+          resize = true,
+          image = recolored_image,
+          widget = wibox.widget.imagebox,
+        },
+        halign = "center",
+        valign = "center",
+        widget = wibox.container.place,
+      },
+      forced_height = 32,
+      forced_width = 32,
+      bg = beautiful.bg_3,
+      shape = gears.shape.circle,
+      widget = wibox.container.background,
+    },
+    right = 16,
+    widget = wibox.container.margin,
+  }
+end
--- ui/toggle-panel/sliders/slider.lua
+++ ui/toggle-panel/sliders/slider.lua
@@ -1,57 +1,31 @@
-local wibox = require("wibox")
-local gears = require("gears")
-local beautiful = require("beautiful")
+local wibox = require "wibox"
+local gears = require "gears"
+local beautiful = require "beautiful"
 
--- slider widget
-local slider = wibox.widget({
-	bar_shape = gears.shape.rounded_rect,
-	bar_height = 4,
-	bar_color = beautiful.grey,
-	bar_active_color = beautiful.fg,
-	--
-	handle_border_width = 2,
-	handle_width = 16,
-	handle_height = 16,
-	handle_shape = gears.shape.circle,
-	handle_color = beautiful.fg,
-	handle_border_color = beautiful.bg_d,
-	--
-	maximum = 100,
-	-- value = 100,
-	forced_height = 17,
-	widget = wibox.widget.slider,
-})
+return function(value, min, max)
+  value = value or 0
+  min = min or 0
+  max = max or 100
 
--- icon widget
-
-local icon = function(image)
-  local recolored_image = gears.color.recolor_image(image, beautiful.fg)
-	return wibox.widget({
-		{
-			{
-				{
-					forced_height = 16,
-					forced_width = 16,
-					resize = true,
-					image = recolored_image,
-					widget = wibox.widget.imagebox,
-				},
-				halign = "center",
-				valign = "center",
-				widget = wibox.container.place,
-			},
-			forced_height = 32,
-			forced_width = 32,
-			bg = beautiful.bg_3,
-			shape = gears.shape.circle,
-			widget = wibox.container.background,
-		},
-		right = 16,
-		widget = wibox.container.margin,
-	})
+  return wibox.widget {
+    bar_shape = gears.shape.rounded_rect,
+    bar_height = 4,
+    bar_color = beautiful.grey,
+    bar_active_color = beautiful.fg,
+    --
+    handle_border_width = 2,
+    handle_width = 16,
+    handle_height = 16,
+    handle_shape = gears.shape.circle,
+    handle_color = beautiful.fg,
+    handle_border_color = beautiful.bg_d,
+    --
+    value = value,
+    minimum = min,
+    maximum = max,
+    forced_height = 17,
+    -- TODO: remove this
+    forced_width = 300,
+    widget = wibox.widget.slider,
+  }
 end
-
-return {
-	icon = icon,
-	slider = slider,
-}
