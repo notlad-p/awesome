be53945de2f10e80fcbc13337f4da592d52e9bad
Dalton <contact@daltonp.dev>
2023-08-11 18:01:34 -0400

    updated brightness slider to change brighness per display

--- ui/toggle-panel/sliders/brightness.lua
+++ ui/toggle-panel/sliders/brightness.lua
@@ -1,45 +1,37 @@
-local awful = require("awful")
-local gears = require("gears")
-local wibox = require("wibox")
-local beautiful = require("beautiful")
+local awful = require "awful"
+local wibox = require "wibox"
+local beautiful = require "beautiful"
 
-local widgets = require("ui.toggle-panel.sliders.slider")
-local icon = widgets.icon(beautiful.brightness)
-local slider = widgets.slider
+-- TODO: setup OSD for brightness value (percentage)
 
-local displays = {}
-local get_displays = function()
-	awful.spawn.easy_async_with_shell("xrandr -q | grep ' connected' | awk '{print $1}'", function(stdout)
-		-- split output and insert into displays table
-		for i in string.gmatch(stdout, "%S+") do
-			table.insert(displays, i)
+return function(s)
+  local icon = require "ui.toggle-panel.sliders.slider-icon" (beautiful.brightness)
+  local slider = require "ui.toggle-panel.sliders.slider" (100)
 
-			-- set brightness to 1 by default
-			awful.spawn("xrandr --output " .. i .. " --brightness 1")
-		end
-	end)
+  local current_display = nil
+  for i in pairs(s.outputs) do
+    current_display = i
+  end
+
+  -- set brightness to 1 (100%) by default
+  if current_display then
+    awful.spawn("xrandr --output " .. current_display .. " --brightness 1")
+  end
+
+  slider:connect_signal("property::value", function()
+    if not current_display then
+      return
+    end
+
+    local value = slider:get_value()
+    local percent = value / 100
+
+    awful.spawn("xrandr --output " .. current_display .. " --brightness " .. percent)
+  end)
+
+  return wibox.widget {
+    icon,
+    slider,
+    layout = wibox.layout.align.horizontal,
+  }
 end
-
-get_displays()
-
-slider:connect_signal("property::value", function()
-	-- get value
-	local value = slider:get_value()
-
-	-- TODO: check if this for loop works, should change brightness of all screens
-	-- set brightness
-	local percent = value / 100
-	for i, v in ipairs(displays) do
-		-- revDays[v] = i
-		awful.spawn("xrandr --output " .. v .. " --brightness " .. percent)
-	end
-end)
-
--- container widget
-local brightness = wibox.widget({
-	icon,
-	slider,
-	layout = wibox.layout.align.horizontal,
-})
-
-return brightness
