f613bb14a36f92c3748074eeb9fdb2b9dd5b1c62
Dalton Perkins <contact@daltonp.dev>
2024-04-09 15:09:02 -0400

    added openweathermap api output to weather widget

--- ui/toggle-panel/weather.lua
+++ ui/toggle-panel/weather.lua
@@ -1,84 +1,110 @@
--- TODO: setup this widget to be connected to a signal & manipulated
--- separate parts out into their own modules? (weather/ directory?)
-
--- TODO: create weather signal like playerctl
--- then update both weather widgets every 5-20 minutes
-
--- TODO: move weather icons to `theme.lua` file
--- so that both weather widgets can use the same icons
 local wibox = require "wibox"
 local gears = require "gears"
 local beautiful = require "beautiful"
+local filesystem = require "gears.filesystem"
 
 local container = require "ui.toggle-panel.container"
+local weather_config = require("configuration.config").widget.weather
+local title_case = require "module.title-case"
+local icon_dir = filesystem.get_configuration_dir() .. "images/icons/weather/"
+local icons = require "module.weather-icons"
 
 local location_icon = gears.color.recolor_image(beautiful.location, beautiful.fg)
+
+local location_string = "N/A"
+
+if weather_config then
+  location_string = title_case(weather_config.city)
+end
+
 local location_text = wibox.widget {
-  markup = "Apex",
+  markup = location_string,
   widget = wibox.widget.textbox,
   font = beautiful.font_name_bold .. " 11",
 }
 
 local temp_text = wibox.widget {
-  markup = "75°",
+  markup = "N/A°",
   widget = wibox.widget.textbox,
   font = beautiful.font_name_bold .. " 48",
 }
 
 local sky_text = wibox.widget {
-  markup = "Clear Sky",
+  markup = "N/A",
   widget = wibox.widget.textbox,
   font = beautiful.font_name_base .. " 11",
 }
 
 local feels_like_text = wibox.widget {
-  markup = "<b>Feels like:</b> 69°",
+  markup = "<b>Feels like:</b> N/A",
   widget = wibox.widget.textbox,
   font = beautiful.font_name_base .. " 11",
 }
 
 local wind_text = wibox.widget {
-  markup = "<b>Wind:</b> 5 mph",
+  markup = "<b>Wind:</b> N/A",
   widget = wibox.widget.textbox,
   font = beautiful.font_name_base .. " 11",
 }
 
 local humidity_text = wibox.widget {
-  markup = "<b>Humidity:</b> 69%",
+  markup = "<b>Humidity:</b> N/A",
   widget = wibox.widget.textbox,
   font = beautiful.font_name_base .. " 11",
 }
 
-local time_weather_widget = wibox.widget {
-  {
+local create_forecast_widget = function()
+  local time_text_widget = wibox.widget {
+    markup = "N/A",
+    widget = wibox.widget.textbox,
+  }
+
+  local weather_icon_widget = wibox.widget {
+
+    image = location_icon,
+    forced_height = 24,
+    forced_width = 24,
+    widget = wibox.widget.imagebox,
+  }
+
+  local temp_text_widget = wibox.widget {
+
+    markup = "N/A",
+    widget = wibox.widget.textbox,
+  }
+
+  local forecast_widget = wibox.widget {
     {
-      markup = "6am",
-      widget = wibox.widget.textbox,
+      time_text_widget,
+      halign = "center",
+      widget = wibox.container.place,
     },
-    halign = "center",
-    widget = wibox.container.place,
-  },
-  {
     {
-      image = location_icon,
-      forced_height = 24,
-      forced_width = 24,
-      widget = wibox.widget.imagebox,
+      weather_icon_widget,
+      halign = "center",
+      widget = wibox.container.place,
     },
-    halign = "center",
-    widget = wibox.container.place,
-  },
-  {
     {
-      markup = "75°",
-      widget = wibox.widget.textbox,
+      temp_text_widget,
+      halign = "center",
+      widget = wibox.container.place,
     },
-    halign = "center",
-    widget = wibox.container.place,
-  },
-  spacing = 4,
-  layout = wibox.layout.fixed.vertical,
-}
+    spacing = 4,
+    layout = wibox.layout.fixed.vertical,
+  }
+
+  return {
+    main = forecast_widget,
+    time_text = time_text_widget,
+    weather_icon = weather_icon_widget,
+    temp_text = temp_text_widget,
+  }
+end
+
+local forecast_widgets = {}
+for i = 1, 5, 1 do
+  forecast_widgets[i] = create_forecast_widget()
+end
 
 local location_header = wibox.widget {
   {
@@ -136,12 +162,10 @@ local weather = wibox.widget {
   {
     {
       {
-        time_weather_widget,
-        time_weather_widget,
-        time_weather_widget,
-        time_weather_widget,
-        -- layout = wibox.layout.align.horizontal,
-        -- layout = wibox.layout.fixed.horizontal,
+        forecast_widgets[1].main,
+        forecast_widgets[2].main,
+        forecast_widgets[3].main,
+        forecast_widgets[4].main,
         layout = wibox.layout.flex.horizontal,
       },
       margins = 16,
@@ -156,5 +180,40 @@ local weather = wibox.widget {
   spacing = 20,
   layout = wibox.layout.fixed.vertical,
 }
+
+-- callback after the command output is received
+local current_weather_callback = function(result)
+  local temp_num = math.floor(result.main.temp)
+  temp_text.markup = tostring(temp_num) .. "°"
+
+  local sky = result.weather[1].description
+  sky_text.markup = title_case(sky)
+
+  local feels_like_num = math.floor(result.main.feels_like)
+  feels_like_text.markup = "<b>Feels like:</b> " .. tostring(feels_like_num) .. "°"
+
+  local wind = math.floor(result.wind.speed)
+  wind_text.markup = "<b>Wind:</b> " .. tostring(wind) .. " mph"
+
+  local humidity = result.main.humidity
+  humidity_text.markup = "<b>Humidity:</b> " .. humidity .. "%"
+end
+
+awesome.connect_signal("current_weather::updated", current_weather_callback)
+
+local forecast_callback = function(result)
+  for i = 1, 5, 1 do
+    forecast_widgets[i].time_text:set_markup_silently(os.date("%I%p", result.list[i].dt))
+
+    local temp = math.floor(result.list[i].main.temp)
+    forecast_widgets[i].temp_text:set_markup_silently(tostring(temp) .. "°")
+
+    local icon_code = result.list[i].weather[1].icon
+    local icon_image = gears.color.recolor_image(icon_dir .. icons[icon_code] .. ".svg", beautiful.fg)
+    forecast_widgets[i].weather_icon.image = icon_image
+  end
+end
+
+awesome.connect_signal("forecast_weather::updated", forecast_callback)
 
 return container(weather, 334)
